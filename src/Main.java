eea07136da2d134ddbed14082cc32ad5c1fac010
LIU-D <943445214@qq.com>
2018-04-22 17:41:44 +0800

    1094

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int N = input.nextInt();
-		for (int i = 0; i < N; i++) {
+		while(input.hasNext()){
 			int M = input.nextInt();
 			int num = 0;
 			for (int j = 0; j < M; j++) {
