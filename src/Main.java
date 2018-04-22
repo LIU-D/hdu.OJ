71e5f522177224a33882b53b4e3ee7b2e47b853e
LIU-D <943445214@qq.com>
2018-04-22 17:10:58 +0800

    1048

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		while(input.hasNext()) {
-			int a, b=0,N;
-			N = input.nextInt();
-			if(N == 0) 
+		while (input.hasNext()) {
+			String s = input.nextLine();
+			if (s.equals("START")) {
+				String line = input.nextLine();
+				StringBuilder sb = new StringBuilder();
+				for (int i = 0; i < line.length(); i++) {
+					if (line.charAt(i) >= 70 && line.charAt(i) <= 90) {
+						char a = (char) (line.charAt(i) - 5);
+						sb.append(a);
+					} else if (line.charAt(i) >= 65 && line.charAt(i) < 70) {
+						char a = (char) (line.charAt(i) + 26 - 5);
+						sb.append(a);
+					} else
+						sb.append(line.charAt(i));
+				}
+				System.out.println(sb);
+			} else if (s.equals("END")) {
+				continue;
+			} else if (s.equals("ENDOFINPUT")) {
 				break;
-			for(int i = 0; i < N; i++) {
-				a = input.nextInt();
-				b += a;		
 			}
-			System.out.println(b);
 		}
 	}
 }
