4df9aa932db767f92967a5fe5b112e69090c761f
LIU-D <943445214@qq.com>
2018-04-22 17:46:06 +0800

    1095
    Sample Input
    1 5
    10 20
    
    Sample Output
    6
    
    30

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while(input.hasNext()){
-			int M = input.nextInt();
-			int num = 0;
-			for (int j = 0; j < M; j++) {
-				int n = input.nextInt();
-				num += n;
-			}
-			System.out.println(num);
+			int a = input.nextInt();
+			int b = input.nextInt();
+			System.out.println(a + b);
+			System.out.println();
 		}
 	}
 }
