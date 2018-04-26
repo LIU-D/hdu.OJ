324b977c1ed82d7e5b0b67a8cb11a93cb8ffd7b0
LIU-D <943445214@qq.com>
2018-04-26 19:08:49 +0800

    2000

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,45 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while (input.hasNext()) {
-			int a = input.nextInt();
-			int b = input.nextInt();
-			if (b == 0)
-				System.out.println(1);
-			else {
-				int aa = a % 10;
-				int bb = b % 4;
-				if (bb == 0) {
-					int count = 1;
-					while (count < 4) {
-						aa *= aa;
-						count++;
-					}
-
-				} else {
-					int count = 1;
-					int aaa = aa;
-					while (count < bb) {
-						aa *= aaa;
-						count++;
-					}
-				}
-				System.out.println(aa % 10);
+			String str = input.nextLine();
+			char[] c = new char[3];
+			char max = 0, mid = 0, min = 0;
+			for (int i = 0; i < str.length(); i++) {
+				c[i] = str.charAt(i);
 			}
+			if (Integer.valueOf(c[0]) > Integer.valueOf(c[1])) {
+				if (Integer.valueOf(c[0]) > Integer.valueOf(c[2])) {
+					max = c[0];
+					if (Integer.valueOf(c[2]) > Integer.valueOf(c[1])) {
+						mid = c[2];
+						min = c[1];
+					} else {
+						mid = c[1];
+						min = c[2];
+					}
+				} else {
+					max = c[2];
+					mid = c[0];
+					min = c[1];
+				}
+			} else if (Integer.valueOf(c[1]) > Integer.valueOf(c[0])) {
+				if (Integer.valueOf(c[1]) > Integer.valueOf(c[2])) {
+					max = c[1];
+					if (Integer.valueOf(c[2]) > Integer.valueOf(c[0])) {
+						mid = c[2];
+						min = c[0];
+					} else {
+						mid = c[0];
+						min = c[2];
+					}
+				} else {
+					max = c[2];
+					mid = c[1];
+					min = c[0];
+				}
+			}
+
+			System.out.println(min + " " + mid + " " + max);
 		}
 	}
 }
