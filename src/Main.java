7c610d266444b10adcdb3ed3436211cfebd6c4f4
LIU-D <943445214@qq.com>
2018-05-02 20:34:55 +0800

    2001

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		while (input.hasNext()) {
-			String str = input.nextLine();
-			char[] c = new char[3];
-			char max = 0, mid = 0, min = 0;
-			for (int i = 0; i < str.length(); i++) {
-				c[i] = str.charAt(i);
-			}
-			if (Integer.valueOf(c[0]) > Integer.valueOf(c[1])) {
-				if (Integer.valueOf(c[0]) > Integer.valueOf(c[2])) {
-					max = c[0];
-					if (Integer.valueOf(c[2]) > Integer.valueOf(c[1])) {
-						mid = c[2];
-						min = c[1];
-					} else {
-						mid = c[1];
-						min = c[2];
-					}
-				} else {
-					max = c[2];
-					mid = c[0];
-					min = c[1];
-				}
-			} else if (Integer.valueOf(c[1]) > Integer.valueOf(c[0])) {
-				if (Integer.valueOf(c[1]) > Integer.valueOf(c[2])) {
-					max = c[1];
-					if (Integer.valueOf(c[2]) > Integer.valueOf(c[0])) {
-						mid = c[2];
-						min = c[0];
-					} else {
-						mid = c[0];
-						min = c[2];
-					}
-				} else {
-					max = c[2];
-					mid = c[1];
-					min = c[0];
-				}
-			}
-
-			System.out.println(min + " " + mid + " " + max);
+		while(input.hasNext()) {
+			Double a = input.nextDouble();
+			Double b = input.nextDouble();
+			Double c = input.nextDouble();
+			Double d = input.nextDouble();
+			Double end = Math.sqrt((a - c) * (a - c) + (b - d) * (b - d));
+			System.out.println(String.format("%.2f",end));
 		}
 	}
 }
