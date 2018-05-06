49986ee8fd90d724423ceeb5d3645fd03d0dfa57
LIU-D <943445214@qq.com>
2018-05-06 15:25:49 +0800

    2005

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		while(input.hasNext()) {
-			Double n = input.nextDouble();
-			System.out.println(String.format("%.2f",Math.abs(n)));
+		Scanner s = new Scanner(System.in);
+		int dd[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		while (s.hasNext()) {
+			int days = 0;
+			String str = s.nextLine();
+			String[] date = str.split("/");
+			int y = Integer.parseInt(date[0]);
+			int m = Integer.parseInt(date[1]);
+			int d = Integer.parseInt(date[2]);
+			if ((y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) && m > 2)
+				days++;
+			days += d;
+			for (int i = 0; i < m; i++) {
+				days += dd[i];
+			}
+			System.out.println(days);
 		}
+		s.close();
 	}
+
 }
