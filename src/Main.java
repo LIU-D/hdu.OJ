ea56e32a2fbf612d5805c4a854d217e711cd3cf2
LIU-D <943445214@qq.com>
2018-05-06 15:13:10 +0800

    2003

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while(input.hasNext()) {
-			Double a = input.nextDouble();
-			Double b = input.nextDouble();
-			Double c = input.nextDouble();
-			Double d = input.nextDouble();
-			Double end = Math.sqrt((a - c) * (a - c) + (b - d) * (b - d));
-			System.out.println(String.format("%.2f",end));
+			Double n = input.nextDouble();
+			System.out.println(String.format("%.2f",Math.abs(n)));
 		}
 	}
 }
