dbf5f42adb80393faa2c3fd912d893678fef4352
LIU-D <943445214@qq.com>
2018-05-10 18:17:55 +0800

    2012

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int m = input.nextInt();
-		for (int i = 0; i < m; i++) {
-			int x = input.nextInt();
-			Double num = (double) 0;
-			if (x < 1000) {
-				for (int j = 1; j <= x; j++) {
-					if (j % 2 == 0)
-						num -= 1.0 / j;
-					else
-						num += 1.0 / j;
+		while(input.hasNext()) {
+			int a = input.nextInt();
+			int b = input.nextInt();
+			int c;
+			if(a == 0 && b == 0) break;
+			boolean flag = true;
+			
+			for(int i = a; i <= b; i++) {
+				c = i * i + i + 41;
+				for(int j = 2; j < c; j++) {
+					if(c % j == 0) {
+						flag = false;
+						break;
+					}
 				}
-				System.out.println(String.format("%.2f", num));
-			}
+				if(!flag) break;
+			}	
+			if(flag) System.out.println("OK");
+			else System.out.println("Sorry");
 		}
-
 	}
 }
