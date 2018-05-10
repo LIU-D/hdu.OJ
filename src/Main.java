f70526a9ece5b116d323914fdebe1a1ad89ce8a4
LIU-D <943445214@qq.com>
2018-05-10 18:26:12 +0800

    2013

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		while(input.hasNext()) {
-			int a = input.nextInt();
-			int b = input.nextInt();
-			int c;
-			if(a == 0 && b == 0) break;
-			boolean flag = true;
-			
-			for(int i = a; i <= b; i++) {
-				c = i * i + i + 41;
-				for(int j = 2; j < c; j++) {
-					if(c % j == 0) {
-						flag = false;
-						break;
-					}
-				}
-				if(!flag) break;
-			}	
-			if(flag) System.out.println("OK");
-			else System.out.println("Sorry");
+		while (input.hasNext()) {
+			int n = input.nextInt();
+			int sum = 1;
+			for (int i = 2; i <= n; i++) {
+				sum = (sum + 1) * 2;
+			}
+			System.out.println(sum);
 		}
 	}
 }
